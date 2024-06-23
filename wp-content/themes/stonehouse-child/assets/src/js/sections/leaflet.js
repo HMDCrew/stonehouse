e9e191fed9812f22311422a3dd5a9b1e43209268
Andrei Leca <15813175+HMDCrew@users.noreply.github.com>
2024-06-23 11:05:21 +0200

    code review (fix code disposition for leaflet code structure)

--- wp-content/themes/stonehouse-child/assets/src/js/sections/leaflet.js
+++ wp-content/themes/stonehouse-child/assets/src/js/sections/leaflet.js
@@ -4,10 +4,9 @@ import L from "leaflet"
 import 'leaflet/dist/leaflet.css'
 
 import { reder_el } from "../utils/helpers"
-import house from '../../images/houses-svgrepo.svg'
 import { leaflet } from "../constants/leaflet"
-import { sendHttpReq } from "../utils/api"
 import { crud } from "../constants/crud"
+import { LeafletUI } from "../layout/leaflet_ui"
 
 let point_marker
 let position_marker
@@ -158,113 +157,5 @@ export const init_map = (init) => {
         map.addEventListener('contextmenu', e => e.preventDefault())
     }
 
-
-    // Saved hauses
-    const housed = document.querySelector('.housed')
-
-    if ( housed ) {
-        leaflet.addController({
-            map: map,
-            icon: house,
-            handler: ( ev ) => housed.parentNode.classList.toggle('show-houses')
-        })
-
-
-        const btns_edits = housed.querySelectorAll('.btn.edit')
-        const btns_save = housed.querySelectorAll('.btn.save')
-        const btns_delets = housed.querySelectorAll('.btn.delete')
-
-
-        const deactivate_old_house_edit = () => {
-
-            const lock_hover = document.querySelector('.info-container.lock_hover')
-
-            if ( lock_hover ) {
-
-                const title = lock_hover.querySelector('.title')
-                const lat = lock_hover.querySelector('.lat span')
-                const lng = lock_hover.querySelector('.lng span')
-                const btn_save = document.querySelector('.btn.save:not(.d-none)')
-                const btn_edit = document.querySelector('.btn.edit.d-none')
-
-                title.removeAttribute('contenteditable', true)
-                lat.removeAttribute('contenteditable', true)
-                lng.removeAttribute('contenteditable', true)
-
-                lock_hover.classList.remove('lock_hover')
-                lock_hover.classList.remove('editable')
-                btn_save.classList.add('d-none')
-                btn_edit.classList.remove('d-none')
-            }
-        }
-
-        const edit_house = ( btn, house_item ) => {
-            
-            deactivate_old_house_edit()
-
-            const info = house_item.querySelector('.info-container')
-            const btn_save = house_item.querySelector('.btn.save')
-            const title = info.querySelector('.title')
-            const lat = info.querySelector('.lat span')
-            const lng = info.querySelector('.lng span')
-
-            if ( !info.classList.contains('lock_hover') ) {
-                title.setAttribute('contenteditable', true)
-                lat.setAttribute('contenteditable', true)
-                lng.setAttribute('contenteditable', true)
-                title.focus()
-            } else {
-                title.removeAttribute('contenteditable', true)
-                lat.removeAttribute('contenteditable', true)
-                lng.removeAttribute('contenteditable', true)
-            }
-
-            // save d-none
-            info.classList.toggle('lock_hover')
-            info.classList.toggle('editable')
-            btn.classList.toggle('d-none')
-            btn_save.classList.toggle('d-none')
-        }
-
-        const delete_house = ( btn, house_item ) => {
-
-            const house_id = house_item.getAttribute('house-id')
-
-            crud.delete_location({house_id}).then(res => {
-
-                res = JSON.parse(res)
-
-                if ( res.status === 'success' ) {
-                    house_item.remove()
-                }
-            })
-        }
-
-        const save_house = ( btn, house_item ) => {
-
-            
-            const house_id = house_item.getAttribute('house-id')
-            const title = house_item.querySelector('.title')
-            const lat = house_item.querySelector('.lat')
-            const lng = house_item.querySelector('.lng')
-            
-            crud.update_location({
-                house_id,
-                title: title.textContent,
-                lat: lat.getAttribute('lat'),
-                lng: lng.getAttribute('lng')
-            }).then(res => {
-
-                res = JSON.parse(res)
-
-                if ( res.status === 'success' ) {
-                    deactivate_old_house_edit()
-                }
-            })
-        }
-
-        btns_edits.forEach( btn => btn.addEventListener( 'click', ev => edit_house( btn, btn.closest('.house') ) ) )
-        btns_delets.forEach( btn => btn.addEventListener( 'click', ev => delete_house( btn, btn.closest('.house') ) ) )
-        btns_save.forEach( btn => btn.addEventListener( 'click', ev => save_house( btn, btn.closest('.house') ) ) )
-    }
+    new LeafletUI(map)
 }
